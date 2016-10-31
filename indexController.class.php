ad3d1b8e22eec5d0b7123665963d081b2724c22a
iuso <759471225@qq.com>
2016-10-31 15:21:18 +0800

    Add files via upload

---
+++ indexController.class.php
@@ -0,0 +1,12 @@
+<?php
+
+	namespace admin\controller;
+	user think\controller;
+
+	class indexcontroller extends commonController 
+	{
+		public function index()
+		{
+			$this->display();
+		}
+	}
